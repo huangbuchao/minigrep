535fe7d72b4c14a4a1afa6b6553a2675cb2600d8
huangbuchao <huangbuchao@gmail.com>
2020-09-28 21:07:01 +0800

    chore: ::snail::refactor

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,21 @@
 use std::env;
-use std::fs;
 use std::process;
+
+mod lib;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("env args: {:?}", args);
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = lib::Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     println!("Searching for: {}", config.query);
     println!("in file: {}", config.filename);
 
-    run(config);
-}
-
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-    println!("With text: \n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = lib::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }

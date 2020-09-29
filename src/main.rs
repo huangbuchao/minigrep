96233a76f756ba6a17583bc3ef5a000bc06acdf2
huangbuchao <huangbuchao@gmail.com>
2020-09-29 17:30:32 +0800

    chore: ::dragon::optimize stderr

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,14 @@ mod lib;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("env args: {:?}", args);
 
     let config = lib::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    println!("Searching for: {}", config.query);
-    println!("in file: {}", config.filename);
 
     if let Err(e) = lib::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
